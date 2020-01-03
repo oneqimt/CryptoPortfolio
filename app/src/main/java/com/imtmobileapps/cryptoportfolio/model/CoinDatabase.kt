4d9c082bac5f325de94b4e61cc35e7478e33c243
oneqimt <oneqimt@gmail.com>
2020-01-03 08:05:33 -0700

    move CoinDao to model package

--- app/src/main/java/com/imtmobileapps/cryptoportfolio/model/CoinDatabase.kt
+++ app/src/main/java/com/imtmobileapps/cryptoportfolio/model/CoinDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.imtmobileapps.cryptoportfolio.CoinDao
 
 @Database(entities = arrayOf(CryptoValue::class), version = 1)
 abstract class CoinDatabase :RoomDatabase(){
