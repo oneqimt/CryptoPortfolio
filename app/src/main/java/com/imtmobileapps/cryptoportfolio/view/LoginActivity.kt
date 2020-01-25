a0d9329aaf55fd5e5d215740effbebd724a41c9e
oneqimt <oneqimt@gmail.com>
2020-01-24 18:48:21 -0700

    added preloader to Login, upgraded to TextInputLayout material design, added error toast

--- app/src/main/java/com/imtmobileapps/cryptoportfolio/view/LoginActivity.kt
+++ app/src/main/java/com/imtmobileapps/cryptoportfolio/view/LoginActivity.kt
@@ -1,6 +1,13 @@
 package com.imtmobileapps.cryptoportfolio.view
 
+import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Gravity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -9,37 +16,77 @@ import com.imtmobileapps.cryptoportfolio.R
 import com.imtmobileapps.cryptoportfolio.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_login.*
 
+
 class LoginActivity : AppCompatActivity() {
-
+    
     private lateinit var viewModel: LoginViewModel
-
+    
     var usernameText = ""
     var passwordText = ""
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-
+        
+        usernameTextInputEditText.setText("")
+        passwordTextInputEditText.setText("")
+        usernameTextInputEditText.requestFocus()
+        
         loginBtn.setOnClickListener {
             validateLogin()
+            it.isEnabled = false
         }
-
+        usernameTextInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    usernameLayout.error = "Please input a valid username."
+                }
+            }
+            
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            
+            }
+            
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            
+            }
+        })
+        
+        passwordTextInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    passwordLayout.error = "Please input a valid password"
+                }
+            }
+            
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            
+            }
+            
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            
+            }
+        })
+        
         observeModel()
     }
-
+    
     fun doLogin() {
-
+        
         viewModel.loginUser(usernameText, passwordText)
     }
-
+    
     fun validateLogin() {
-
-        usernameText = username.text.toString()
-        passwordText = password.text.toString()
-
+        
+        usernameText = usernameTextInputEditText.text.toString()
+        passwordText = passwordTextInputEditText.text.toString()
+        
+        
         if (usernameText.isNotEmpty() and passwordText.isNotEmpty()) {
+            loading.visibility = View.VISIBLE
+            loginBtn.isEnabled = true
             doLogin()
         } else {
             Toast.makeText(
@@ -47,41 +94,69 @@ class LoginActivity : AppCompatActivity() {
                 "Please enter Username and Password",
                 Toast.LENGTH_SHORT
             ).show()
-
+            
         }
-
+        
     }
-
+    
     fun observeModel() {
-
+        
         viewModel.user.observe(this, Observer { user ->
             user?.let {
                 println("$TAG USER in NOT NULL")
+                loading.visibility = View.GONE
+                loginBtn.isEnabled = true
             }
-
+            
         })
-
+        
         viewModel.loginError.observe(this, Observer { isError ->
             isError?.let {
                 if (isError) {
-                    username.setText("")
-                    password.setText("")
-                    username.requestFocus()
-                    // notify user
-                    Toast.makeText(
+                    val imm: InputMethodManager = this
+                        .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+                    loading.visibility = View.GONE
+                    loginBtn.isEnabled = true
+                    
+                    usernameTextInputEditText.setText("")
+                    passwordTextInputEditText.setText("")
+                    usernameTextInputEditText.clearFocus()
+                    passwordTextInputEditText.clearFocus()
+                    
+                    val toastDurationInMilliSeconds = 10000L
+                    val t = Toast.makeText(
                         getApplication(),
-                        "There was a login error. Please try again.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-
+                        "There was a server error with those login values. Please try again.",
+                        Toast.LENGTH_LONG
+                    )
+                    t.setGravity(Gravity.TOP, 0, 300)
+                    val toastCountDown: CountDownTimer
+                    toastCountDown = object :
+                        CountDownTimer(
+                            toastDurationInMilliSeconds,
+                            1000
+                        ) {
+                        override fun onTick(millisUntilFinished: Long) {
+                            t.show()
+                        }
+                        
+                        override fun onFinish() {
+                            t.cancel()
+                        }
+                    }
+                    
+                    t.show();
+                    toastCountDown.start();
+                    
                 }
-
+                
             }
         })
     }
-
+    
     companion object {
         private val TAG = LoginActivity::class.java.simpleName
     }
-
+    
 }
